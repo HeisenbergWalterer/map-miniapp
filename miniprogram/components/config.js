7fafd188b1a290bf8e38ca6031ac34bc7ca173ba
skw <sunkaiwen8@gmail.com>
2025-07-25 23:23:59 +1200

    git push map.js

--- miniprogram/components/config.js
+++ miniprogram/components/config.js
@@ -1,4 +1,5 @@
-const config={
-  key:'bb2e9ae6dafd0e5b92a4c3ca05ed67dd'
+const config = {
+  key: 'bb2e9ae6dafd0e5b92a4c3ca05ed67dd' 
 }
+
 module.exports.Config = config;
--- miniprogram/pages/map/map.js
+++ miniprogram/pages/map/map.js
@@ -1,6 +1,7 @@
-var gaode_key = require ('../../components/config')
+var gaode_key = require('../../components/config')
 var amapFile = require('../../components/amap-wx.130');
 var markersData = []
+
 Page({
   data: {
     markers: [],
@@ -20,38 +21,93 @@ Page({
     distance: '', //导航总距离
     daohang: false, //是否开始导航
     mapState: true, //目的地搜索状态
+    searchKeyword: '', //搜索关键词
   },
-  onload:function(){
+
+  onLoad: function(){
     this.getPoiData() //获取当前位置
   },
+
+  onReady: function(){
+    console.log('地图页面初次渲染完成')
+  },
+
+  onShow: function(){
+    console.log('地图页面显示')
+  },
+
+  onHide: function(){
+    console.log('地图页面隐藏')
+  },
+
+  onUnload: function(){
+    console.log('地图页面卸载')
+  },
+
+  // 搜索输入
+  inputSearch: function(e) {
+    this.setData({
+      searchKeyword: e.detail.value
+    });
+  },
+
+  // 搜索附近
+  searchNearby: function() {
+    if (this.data.searchKeyword) {
+      this.getPoiData(this.data.searchKeyword);
+    }
+  },
+
   getPoiData: function(keywords){
     var that = this;
-    let params ={
-    success: function (data){
-      console.log('当前位置',data)
-      markersData = data.markers; //标记的点位
-      console.log('搜索当前位置，清除目的地信息')
-      that.serData({
-        markers: markersData,
-        latitude:markersData[0].latitude,
-        longitude:markersData[0].longitude,
-        city: markersData[0].name,
-      });
-      that.showMarkerInfo(markersData,0);
-    },
-    fail: function(info){
-      wx.showModal({
-        title:info.errMsg
-      })
+    let params = {
+      success: function (data){
+        console.log('搜索结果',data)
+        markersData = data.markers; //标记的点位
+        
+        that.setData({
+          markers: markersData,
+          latitude: markersData[0].latitude,
+          longitude: markersData[0].longitude,
+          city: markersData[0].name,
+        });
+        that.showMarkerInfo(markersData, 0);
+      },
+      fail: function(info){
+        console.error('获取位置失败：', info);
+        wx.showModal({
+          title: '提示',
+          content: info.errMsg || '获取位置信息失败'
+        })
+      }
     }
-  }
-    if(keyboards){ //待完成搜索目标附近点位
-      Params.querykeyword = keyboards;
+
+    if(keywords){ //搜索目标附近点位
+      params.querykeywords = keywords;
     }
+
     var myAmapFun = new amapFile.AMapWX({
       key: gaode_key.Config.key
     });
     myAmapFun.getPoiAround(params)
+  },
+
+  // 显示标记信息
+  showMarkerInfo: function(markersData, index) {
+    var that = this;
+    if (markersData && markersData[index]) {
+      that.setData({
+        textData: {
+          name: markersData[index].name || '位置点',
+          desc: markersData[index].desc || markersData[index].address || '详细地址信息'
+        }
+      });
+    }
+  },
+
+  // 标记点击事件
+  markertap: function(e) {
+    var id = e.detail.markerId;
+    this.showMarkerInfo(markersData, id);
   }
- 
 })
