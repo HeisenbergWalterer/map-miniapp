e472fe87447010d91a2cfa6ec400416a2251936e
skw <sunkaiwen8@gmail.com>
2025-07-25 21:01:15 +1200

    地图调用
    已经可以显示用户的实时位置以及地图信息

---
+++ miniprogram/components/config.js
@@ -0,0 +1,4 @@
+const config={
+  key:'bb2e9ae6dafd0e5b92a4c3ca05ed67dd'
+}
+module.exports.Config = config;
--- miniprogram/pages/map/map.js
+++ miniprogram/pages/map/map.js
@@ -1,26 +1,57 @@
-// map.js
+var gaode_key = require ('../../components/config')
+var amapFile = require('../../components/amap-wx.130');
+var markersData = []
 Page({
   data: {
-    
+    markers: [],
+    latitude: '',
+    longitude: '',
+    city: '',
+    city_e: '', //目的地
+    latitude_e: '', //目的地x
+    longitude_e: '',//目的地y
+    textData:{}, //地点描述信息
+    gaode_type: 'car', //默认驾车导航，后续可改为步行或者公交
+    polyline: [],
+    includePoints: [],
+    transits: [], //公交车信息
+    mapEndObj: {}, //目的地信息
+    cost: '', //打车费用
+    distance: '', //导航总距离
+    daohang: false, //是否开始导航
+    mapState: true, //目的地搜索状态
   },
-
-  onLoad() {
-    console.log('地图页面加载')
+  onload:function(){
+    this.getPoiData() //获取当前位置
   },
-
-  onReady() {
-    console.log('地图页面初次渲染完成')
-  },
-
-  onShow() {
-    console.log('地图页面显示')
-  },
-
-  onHide() {
-    console.log('地图页面隐藏')
-  },
-
-  onUnload() {
-    console.log('地图页面卸载')
+  getPoiData: function(keywords){
+    var that = this;
+    let params ={
+    success: function (data){
+      console.log('当前位置',data)
+      markersData = data.markers; //标记的点位
+      console.log('搜索当前位置，清除目的地信息')
+      that.serData({
+        markers: markersData,
+        latitude:markersData[0].latitude,
+        longitude:markersData[0].longitude,
+        city: markersData[0].name,
+      });
+      that.showMarkerInfo(markersData,0);
+    },
+    fail: function(info){
+      wx.showModal({
+        title:info.errMsg
+      })
+    }
   }
+    if(keyboards){ //待完成搜索目标附近点位
+      Params.querykeyword = keyboards;
+    }
+    var myAmapFun = new amapFile.AMapWX({
+      key: gaode_key.Config.key
+    });
+    myAmapFun.getPoiAround(params)
+  }
+ 
 })
